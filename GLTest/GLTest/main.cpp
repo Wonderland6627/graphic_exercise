bfbe6ddf99a5b98ed3f05ef03a9ae2811a60bc6a
jianinghe <994015020@qq.com>
2021-07-30 11:02:46 +0800

    GLSL学习（in/out/uniform）

--- GLTest/GLTest/main.cpp
+++ GLTest/GLTest/main.cpp
@@ -4,25 +4,33 @@
 
 using namespace std;
 
-const char* vertexShaderSource =	"#version 330 core\n"
-									"layout(location = 0) in vec3 aPos;\n"
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 800;
+
+const char* vertexShaderSource =	"#version 330 core\n"//顶点着色器
+									"layout(location = 0) in vec3 aPos;\n"//位置变量的属性位置值为0
+									"layout(location = 1) in vec3 aColor;\n"//颜色变量的属性位置值为1
+								    "out vec3 vertexColor;\n"//向片段着色器输出一个颜色 接收要同名同类型
 									"void main()\n"
 									"{\n"
-									"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+									"	gl_Position = vec4(aPos, 1.0);\n"
+									"	vertexColor = aColor;\n"//将vertexColor设置为从顶点数据获得的输入颜色
 									"}\0";
 
-const char* fragmentShaderSource =	"#version 330 core\n"
+const char* fragmentShaderSource =	"#version 330 core\n"//片段着色器1
 									"out vec4 FragColor;\n"
+									"in vec3 vertexColor;\n"//接收顶点着色器输入的颜色
 									"void main()\n"
 									"{\n"
-									"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+									"	FragColor = vec4(vertexColor,1);\n"
 									"}\0";
 
 const char* fragmentShaderSource2 =	"#version 330 core\n"
 									"out vec4 FragColor;\n"
+									"uniform vec4 outColor;\n"
 									"void main()\n"
 									"{\n"
-									"	FragColor = vec4(0.0f, 1.0f, 0.2f, 1.0f);\n"
+									"	FragColor = outColor;\n"
 									"}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -35,7 +43,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);//核心模式(Core-profile)
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "MyOpenGLWindow", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "MyOpenGLWindow", NULL, NULL);
 	if (window == NULL)
 	{
 		cout << "Failed to create GLFW window" << endl;
@@ -130,9 +138,14 @@ int main()
 		-0.5f, -0.5f, 0.0f, // 左下角
 		-0.5f, 0.5f, 0.0f   // 左上角*/
 
-		0,0,0,
+		/*0,0,0,
 		0.5f, 0, 0,
-		0, 0.5f, 0,
+		0, 0.5f, 0,*/
+
+		// 位置              // 颜色
+		0.5f, -0.5f, 0.0f,	 1.0f, 0.0f, 0.0f,   // 右下
+		-0.5f, -0.5f, 0.0f,	  0.0f, 1.0f, 0.0f,   // 左下
+		0.0f,  0.5f, 0.0f,	 0.0f, 0.0f, 1.0f    // 顶部
 	};
 
 	float vertices2[] =
@@ -165,8 +178,10 @@ int main()
 	/*glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);//把索引复制进缓冲
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);*/
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glBindVertexArray(VAO[1]);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
@@ -198,11 +213,17 @@ int main()
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glUseProgram(shaderProgram2);
+
+		float timeValue = glfwGetTime();//运行的秒数
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram2, "outColor");//定位uniform变量outColor 返回-1代表没找到
+		glUniform4f(vertexColorLocation, 0, greenValue, 0, 1);//更新uniform值，必须在更新之前UseProgram
+
 		glBindVertexArray(VAO[1]);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		glfwSwapBuffers(window);//检测触发事件
-		glfwPollEvents();//交换颜色缓冲（双缓冲）
+		glfwSwapBuffers(window);//交换颜色缓冲（双缓冲）
+		glfwPollEvents();//检测触发事件
 	}
 
 	glDeleteVertexArrays(2, VAO);
