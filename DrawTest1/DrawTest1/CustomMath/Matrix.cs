efc760976c808f75f94099a3fe77b0a7faa82811
jianinghe <994015020@qq.com>
2021-08-07 12:01:49 +0800

    数学方法

---
+++ DrawTest1/DrawTest1/CustomMath/Matrix.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrawTest1.CustomMath
+{
+    public class Matrix
+    {
+        private float[,] matrix;
+
+        public int rows;
+
+        public int coloumns;
+
+        public Matrix(int rows, int columns)
+        {
+            matrix = new float[rows, columns];
+            this.rows = rows;
+            this.coloumns = columns;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = 1;
+                }
+            }
+        }
+
+        public Matrix(float[,] ms, int rows, int columns)
+        {
+            matrix = new float[rows, columns];
+            this.rows = rows;
+            this.coloumns = columns;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = ms[i, j];
+                }
+            }
+        }
+
+        public float this[int row, int coloumns]
+        {
+            get
+            {
+                return matrix[row, coloumns];
+            }
+        }
+
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            float[,] multi = new float[a.rows, b.coloumns];
+
+            if (a.coloumns == b.rows)
+            {
+                for (int i = 0; i < a.rows; i++)
+                {
+                    for (int j = 0; j < b.coloumns; j++)
+                    {
+                        for (int k = 0; k < a.coloumns; k++)
+                        {
+                            multi[i, j] += a[i, k] * b[k, j]; //第i行j列的值为a的第i行上的n个数和b的第j列上的n个数对应相乘之和，其中n为a的列数，也是b的行数，a的列数和b的行数相等
+                        }
+                    }
+                }
+            }
+
+            return new Matrix(multi, a.rows, b.coloumns);
+        }
+    }
+}
---
+++ DrawTest1/DrawTest1/CustomMath/Vector3.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrawTest1.CustomMath
+{
+    public class Vector3
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Vector3()
+        {
+
+        }
+
+        public Vector3(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Vector3 zero => new Vector3(0, 0, 0);
+
+        public static Vector3 one => new Vector3(1, 1, 1);
+
+        public static Vector3 right => new Vector3(1, 0, 0);
+
+        public static Vector3 up => new Vector3(0, 1, 0);
+
+        public static Vector3 forward => new Vector3(0, 0, 1);
+
+        public float Length()
+        {
+            return (float)Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        public Vector3 Normalize()
+        {
+            return this * (1 / Length());
+        }
+
+        public static float Dot(Vector3 left, Vector3 right)
+        {
+            return left.x * right.x + left.y * right.y + left.z * right.z;
+        }
+
+        public static Vector3 Cross(Vector3 left, Vector3 right)
+        {
+            return new Vector3(left.y * right.z - left.z * right.y,
+                               left.z * right.x - left.x * right.z,
+                               left.x * right.y - left.y * right.x);
+        }
+
+        public static Vector3 operator + (Vector3 left, Vector3 right)
+        {
+            return new Vector3(left.x + right.x, left.y + right.y, left.z + right.z);
+        }
+
+        public static Vector3 operator -(Vector3 left, Vector3 right)
+        {
+            return new Vector3(left.x - right.x, left.y - right.y, left.z - right.z);
+        }
+
+        public static Vector3 operator * (Vector3 vector, float multi)
+        {
+            return new Vector3(vector.x * multi, vector.y * multi, vector.z * multi);
+        }
+    }
+}
